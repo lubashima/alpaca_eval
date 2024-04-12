024ff46f4693c8c4f89f744f9c0567907eedf12f
lubashima <luisashimabucoro@cohere.com>
2024-04-12 20:07:51 +0000

    Add: new config files created by AlpacaEval to use new models

--- src/alpaca_eval/metrics/winrate.py
+++ src/alpaca_eval/metrics/winrate.py
@@ -8,6 +8,14 @@ from .helpers import AbsoluteScoringRule, ZeroOneScoringRule
 
 __all__ = ["get_winrate", "pairwise_to_winrate"]
 
+def is_longer(generation_one, generation_two):
+    if len(generation_one) > len(generation_two):
+        return 1.0 
+    else:
+        return 2.0
+    
+def get_first_order_bias(annotations):
+    return annotations.raw_completion.str.contains('Output \(a\)').sum()
 
 def get_winrate(annotations: Union[pd.DataFrame, Sequence]) -> dict[str, float]:
     """Extract head2head metrics (n_wins, n_counts, win_rate) from a sequence preference.
@@ -15,8 +23,17 @@ def get_winrate(annotations: Union[pd.DataFrame, Sequence]) -> dict[str, float]:
     """
     annotations = utils.convert_to_dataframe(annotations)
     preferences = annotations["preference"]
+    print(annotations)
+    annotations['compare_length'] = [is_longer(a, b) for a, b in zip(annotations['output_1'], annotations['output_2'])]
     out = AbsoluteScoringRule().describe_head2head(preferences)
+    print(f"Matches: {get_first_order_bias(annotations)}")
+    out['first_order_preference'] = get_first_order_bias(annotations) / out['n_total']
+    out["reference"] = annotations['generator_1'].unique()[0]
+    out["generator"] = annotations['generator_2'].unique()[0]
     out["discrete_win_rate"] = ZeroOneScoringRule().describe_head2head(preferences)["win_rate"]
+    out["length_bias"] = sum(annotations['compare_length'] == annotations['preference']) / out['n_total']
+    out["ego_bias"] = out['n_wins_base'] / out['n_total']
+    print(out)
     return out
 
 
